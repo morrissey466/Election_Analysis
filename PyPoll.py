e297fb560e1fa882a9668285c54c118e4ea514b4
John Morrissey <morrissey466@gmail.com>
2022-03-15 21:12:07 -0500

    adding files

--- PyPoll.py
+++ PyPoll.py
@@ -4,10 +4,16 @@
 # the total number of bvotes each candidate won 
 # the winner of the election based on popular vote 
 
+import csv
+import os
 
-file_to_load = 'resources/election_results.csv'
-election_data = open(file_to_load,'r')
-election_data.close()
+file_to_load = os.path.join("resources", "election_results.csv")
+election_data = open(file_to_load, 'r')
+with open(file_to_load) as election_data:
+    
+# To do: read and analyze the data here
 
+    file_reader = csv.reader(election_data)
 
-
+    headers = next(file_reader)
+    print(headers)
--- Python_Practice.py
+++ Python_Practice.py
@@ -1,3 +1,2 @@
-counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
-for county, voters in counties_dict.items():
-    print(county + " county has " + str(voters) + " registered voters.")
+open(resources/electon_results.csv, 'r')
+
