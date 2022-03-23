3ee9c0f2816d3d06efe468f6f017fa9d1390f27f
John Morrissey <morrissey466@gmail.com>
2022-03-23 18:47:40 -0500

    adding

--- PyPoll.py
+++ PyPoll.py
@@ -4,16 +4,18 @@
 # the total number of bvotes each candidate won 
 # the winner of the election based on popular vote 
 
+# Add our dependencies.
 import csv
 import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-file_to_load = os.path.join("resources", "election_results.csv")
-election_data = open(file_to_load, 'r')
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
-    
-# To do: read and analyze the data here
-
     file_reader = csv.reader(election_data)
 
+    # Read and print the header row.
     headers = next(file_reader)
     print(headers)
--- Python_Practice.py
+++ Python_Practice.py
@@ -1,2 +1,10 @@
 open(resources/electon_results.csv, 'r')
 
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Hello World")
